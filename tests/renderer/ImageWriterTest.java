64406cf0ccd5ad6efbe6ce014969de870a228669
רננה עמנואל <remanuel@g.jct.ac.il>
2021-04-18 12:22:59 +0300

    exercise 4

--- tests/renderer/ImageWriterTest.java
+++ tests/renderer/ImageWriterTest.java
@@ -12,10 +12,19 @@ class ImageWriterTest {
         ImageWriter imageWriter = new ImageWriter("testblue", 800, 500);
         for(int i=0; i<800; i++) {
             for (int j = 0; j < 500; j++) {
-                imageWriter.writePixel(i, j, new Color(0d,0d,255d));
+                if(i%50 == 0 || j%50 == 0){
+                    imageWriter.writePixel(i, j, Color.BLACK);
+                }
+                else{
+                    imageWriter.writePixel(i, j, new Color(0d,0d,255d));
+
+                }
             }
+
+
         }
         imageWriter.writeToImage();
     }
+
 
 }
